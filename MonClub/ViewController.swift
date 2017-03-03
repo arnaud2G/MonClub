868b6cc3ab7db529679e2fa481c19c38bad854f6
arnaud2G <arnaud.verrier@gmail.com>
2017-03-03 10:20:55 +0100

    Initial Commit

---
+++ MonClub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MonClub
+//
+//  Created by 2Gather Arnaud Verrier on 03/03/2017.
+//  Copyright © 2017 Arnaud Verrier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MonClubTests/MonClubTests.swift
@@ -0,0 +1,36 @@
+//
+//  MonClubTests.swift
+//  MonClubTests
+//
+//  Created by 2Gather Arnaud Verrier on 03/03/2017.
+//  Copyright © 2017 Arnaud Verrier. All rights reserved.
+//
+
+import XCTest
+@testable import MonClub
+
+class MonClubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
